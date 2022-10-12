ad6b2a4413b1596312e66613760a146ea0b5d29a
Kwangjin park <hijin23@hotmail.com>
2022-10-12 13:41:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = load_model('final_tuned_rf')
 
 def predict(model, input_df):
     predictions_df = predict_model(estimator=model, data=input_df)
-    predictions = predictions_df['Label'][0]
+    predictions = predictions_df['Delay'][0]
     return predictions
 
 def run():
