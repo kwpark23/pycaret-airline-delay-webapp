dc6dcd7d8d59a48e23059e6f15c932093d2481c3
Kwangjin park <hijin23@hotmail.com>
2022-10-13 13:13:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,14 +32,37 @@ def run():
 
     if add_selectbox == 'Online':
         
-        flight = st.number_input('Flight', min_value=10, max_value=2000, value=10)
-        airline = st.selectbox('Airline', ['CO', 'US', 'AA','AS','DL','WN','OO','DL','EV','XE','OH'])
-        dayofweek = st.number_input('Day of Week', min_value=1, max_value=7, value=1)
-        airportfrom = st.selectbox('Airport From', ['SFO','PHX','LAX','SFO','ANC','LAX','DEN','ONT','EWR','LIT','SAV','ECP','CHA'])
-        airportto = st.selectbox('Airport To', ['SFO','PHX','LAX','SFO','ANC','LAX','DEN','ONT','EWR','LIT','SAV','ECP','CHA'])
-        time = st.number_input('Time Departed', min_value=10, max_value=1440, value=10)
-        length = st.number_input('Length of Flight', min_value=10, max_value=600, value=10)
+        flight = st.number_input('Flight Number (Enter a value between 10 and 2000)', min_value=10, max_value=2000, value=10)
+        airline = st.selectbox('Airline', ['CO', 'AA','AS','DL','WN','OO','DL','EV','XE','OH'])
+        st.text('CO: North-Western Cargo International Airlines')
+        st.text('AA: American Airlines')
+        st.text('AS: Alaska Airlines')
+        st.text('DL: Delta AirLines')
+        st.text('WN: Southwest Airlines')
+        st.text('OO: SkyWest Airlines')
+        st.text('EV: ExpressJet Airlines')
+        st.text('XE: Delux Public Charter LLC dba JSX Air')
+        st.text('OH: PSA Airlines')
+
+        dayofweek = st.number_input('Day of Week (1 is Monday and 7 is Sunday)', min_value=1, max_value=7, value=1)
+        airportfrom = st.selectbox('Airport From', ['SFO','PHX','LAX','ANC','DEN','ONT','EWR','LIT','SAV','CHA'])
+        st.text('SFO: San Francisco, CA ')
+        st.text('PHX: Phoenix, AZ')
+        st.text('LAX: Los Angeles, CA')
+        st.text('ANC: Anchorage, AK')
+        st.text('DEN: Denver, CO')
+        st.text('ONT: Ontario, CA')
+        st.text('EWR: Newark, NJ')
+        st.text('LIT: Little Rock, AR')
+        st.text('SAV: Savannah, GA')
+        st.text('CHA: Chattanooga, TN')
         
+        airportto = st.selectbox('Airport To', ['SFO','PHX','LAX','ANC','DEN','ONT','EWR','LIT','SAV','CHA'])
+        time = st.number_input('Time Departed (Enter a value between 10 and 1440)', min_value=10, max_value=1440, value=10)
+        st.text('For example, if the flight departed at 7pm, your input should be 420 (19th hour x 60 minutes)')
+        length = st.number_input('Length of Flight', min_value=10, max_value=600, value=10)
+        st.text('For example, if the flight lasted for 3 hours, your input should be 180 (3 x 60 minutes)')
+
         output=""
 
         ids = random.randint(1, 100000)
